a621f976f52d723651bdd24dfaa6d8056511baee
Luis Fernando Antonio Toral <luislecompt@gmail.com>
2021-11-11 10:48:41 -0600

    Término de sección

--- src/tests/components/search/SearchScreen.test.js
+++ src/tests/components/search/SearchScreen.test.js
@@ -30,5 +30,44 @@ describe('Pruebas en SearchScreen', () => {
         expect(wrapper.find('input').prop('value')).toBe('batman');
         expect(wrapper).toMatchSnapshot();
     });
-        
+    
+    test('debe de mostrar un error si no se encuentra el Hero', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <Route
+                        path="/search"
+                        component={ SearchScreen }
+                />
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('.alert-danger').text().trim()).toBe('There is no hero with batman123.');
+    });
+    
+    test('debe llamar el push del history', () => {
+        const history = {
+            push: jest.fn(),
+        }
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <Route
+                        path="/search"
+                        component={ () => <SearchScreen history={ history } /> }
+                />
+            </MemoryRouter>
+        );
+
+        wrapper.find('input').simulate('change',  { 
+            target: { 
+                name: 'search', 
+                value: 'batman' 
+            }
+        });
+
+        wrapper.find('form').prop('onSubmit')({ preventDefault(){} });
+
+        expect(history.push).toHaveBeenCalledWith('?q=batman');
+    });
+    
 });
